16f59ab04eb34ae758afa8e0ee32147ad02e4f0c
tiantian <1016077342@qq.com>
2019-04-19 18:28:50 +0800

    月历修改

--- app/src/main/java/com/project/viewtest/activity/CalendarActivity.java
+++ app/src/main/java/com/project/viewtest/activity/CalendarActivity.java
@@ -1,15 +1,32 @@
 package com.project.viewtest.activity;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.project.viewtest.R;
+import com.project.viewtest.databinding.ActivityCalendarBinding;
 
 public class CalendarActivity extends AppCompatActivity {
+
+    private int month = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendar);
+        ActivityCalendarBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_calendar);
+        binding.add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        binding.remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/project/viewtest/widget/CalendarItemView.java
+++ app/src/main/java/com/project/viewtest/widget/CalendarItemView.java
@@ -6,6 +6,7 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -29,6 +30,9 @@ public class CalendarItemView extends View {
     private int itemW;
     private int padding;
     private int margin;
+    private int currData = -1;
+    private int clickData = -1;
+    private int firstDayForWeek;
 
     public CalendarItemView(Context context) {
         super(context);
@@ -57,7 +61,7 @@ public class CalendarItemView extends View {
         day = array.getInt(R.styleable.CalendarItemView_day, mDay);
         array.recycle();
         if (month <= 7) {
-            if (month == 1) {
+            if (month == 2) {
                 dayCount = 28 + ((year % 4 == 0) ? 1 : 0);
             } else {
                 dayCount = (month % 2 == 0) ? 30 : 31;
@@ -68,6 +72,7 @@ public class CalendarItemView extends View {
         c.set(year, month - 1, day);
         Log.e("aaa", "init: " + year + "/" + month + "/" + day);
         weekDay = c.get(Calendar.DAY_OF_WEEK);
+        firstDayForWeek = weekDay - day % 7 + 1;
         paint = new Paint();
         paint.setColor(Color.RED);
         paint.setTextSize(getContext().getResources().getDisplayMetrics().scaledDensity * 15);
@@ -82,15 +87,27 @@ public class CalendarItemView extends View {
                     boolean end = rect.contains(endX, endY);
                     if (start && end) {
                         Log.e("aaa", "onClick: ok");
+//                        Path path = new Path();
+//                        path.moveTo(rect.left, rect.top);
+//                        path.lineTo(rect.right, rect.top);
+//                        path.lineTo(rect.right, rect.bottom);
+//                        path.lineTo(rect.left, rect.bottom);
+//                        path.close();
+                        clickData = i;
+                        invalidate();
+                        break;
                     }
                     Log.e("aaa", "onClick: " + i);
                     i++;
                 }
             }
         });
+        setBackgroundColor(Color.CYAN);
     }
 
     Rect[] dayRect;
+
+    private int color[] = new int[]{Color.YELLOW, Color.BLACK, Color.BLUE, Color.GRAY, Color.GREEN, Color.WHITE, Color.CYAN};
 
     @SuppressLint("DrawAllocation")
     @Override
@@ -100,6 +117,7 @@ public class CalendarItemView extends View {
         int x;
         int y;
         dayRect = new Rect[dayCount];
+        int weekDay = firstDayForWeek;
         for (int i = weekDay; i < dayCount + weekDay; i++) {
             String dayNum = String.valueOf(i - weekDay + 1);
             paint.getTextBounds(dayNum, 0, dayNum.length(), rect);
@@ -107,7 +125,7 @@ public class CalendarItemView extends View {
             int half = (fontMetricsInt.descent - fontMetricsInt.ascent) / 2;
             if (i <= 7) {
                 if (h == 0)
-                    h += padding;
+                    h += padding / 2;
                 x = i * itemW - itemW / 2 - rect.width() / 2;
             } else {
                 int a = i % 7;
@@ -117,8 +135,16 @@ public class CalendarItemView extends View {
                 x = (a == 0 ? 7 : a) * itemW - itemW / 2 - rect.width() / 2;
             }
             y = h + (half - fontMetricsInt.descent);
-            dayRect[Integer.valueOf(dayNum) - 1] = new Rect(x - itemW, h, x, h + padding);
+            dayRect[Integer.valueOf(dayNum) - 1] = new Rect(x - itemW / 2 + rect.width() / 2, h - padding / 2, x + itemW / 2 + rect.width() / 2, h + padding / 2);
             Log.e("aaa", "onDraw: " + x + "/" + y + "/" + weekDay + "/" + i + "/" + dayNum);
+//            if (Integer.valueOf(dayNum) - 1 == clickData) {
+                paint.setColor(color[i % 6]);
+                canvas.drawRect(dayRect[Integer.valueOf(dayNum) - 1], paint);
+//            }
+            paint.setColor(Color.RED);
+            if (day == Integer.valueOf(dayNum)) {
+                paint.setColor(Color.BLACK);
+            }
             canvas.drawText(dayNum, x, y, paint);
         }
     }
@@ -130,7 +156,7 @@ public class CalendarItemView extends View {
         int w = MeasureSpec.getSize(widthMeasureSpec);
         int h = MeasureSpec.getSize(heightMeasureSpec);
         int width = w;
-        int height = (int) (getContext().getResources().getDisplayMetrics().density * 350 + 0.5f);
+        int height = padding * 7;
         itemW = width / 7;
         setMeasuredDimension(wMode == MeasureSpec.EXACTLY ? w : width, hMode == MeasureSpec.EXACTLY ? h : height);
     }
@@ -145,10 +171,17 @@ public class CalendarItemView extends View {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             startX = (int) event.getX();
             startY = (int) event.getY();
-        } else if (MotionEvent.ACTION_DOWN == event.getAction()) {
+            Log.e("aaa", "dispatchTouchEvent: down - " + startX + "/" + startY);
+        } else if (MotionEvent.ACTION_UP == event.getAction()) {
             endX = (int) event.getX();
             endY = (int) event.getY();
+            Log.e("aaa", "dispatchTouchEvent: up - " + endX + "/" + endY);
         }
         return super.dispatchTouchEvent(event);
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+        invalidate();
     }
 }
