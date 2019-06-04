e971b189d0d0a8985cfed3f6d06b61a6f9b0e0ce
tiantian <1016077342@qq.com>
2019-06-04 18:15:07 +0800

    微改

--- app/src/main/java/com/project/viewtest/photo/ShowPhotoActivity.java
+++ app/src/main/java/com/project/viewtest/photo/ShowPhotoActivity.java
@@ -1,14 +1,13 @@
 package com.project.viewtest.photo;
 
+import android.animation.ValueAnimator;
 import android.databinding.DataBindingUtil;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.ViewGroup;
+import android.util.Log;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.DrawableImageViewTarget;
@@ -17,25 +16,60 @@ import com.project.viewtest.databinding.ActivityShowPhotoBinding;
 
 public class ShowPhotoActivity extends FragmentActivity {
 
+    private ActivityShowPhotoBinding binding;
+    private int width;
+    private int height;
+    private int left;
+    private int top;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final ActivityShowPhotoBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_show_photo);
-        int left = getIntent().getIntExtra("left", 0);
-        int top = getIntent().getIntExtra("top", 0);
-        int width = getIntent().getIntExtra("width", 0);
-        int height = getIntent().getIntExtra("height", 0);
-        binding.image.setX(left);
-        binding.image.setY(top);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_show_photo);
+        left = getIntent().getIntExtra("left", 0);
+        top = getIntent().getIntExtra("top", 0);
+        width = getIntent().getIntExtra("width", 0);
+        height = getIntent().getIntExtra("height", 0);
+        binding.image.setLeft(left);
+        binding.image.setTop(top);
         ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(width, height);
         binding.image.setLayoutParams(layoutParams);
         Glide.with(this).load(getIntent().getStringExtra("url")).into(new DrawableImageViewTarget(binding.image) {
             @Override
             protected void setResource(@Nullable Drawable resource) {
                 super.setResource(resource);
-                binding.image.setScaleX(3);
-                binding.image.setScaleY(3);
+                show();
             }
         });
+    }
+
+    private void show() {
+        final float ww = getResources().getDisplayMetrics().widthPixels;
+        final float wh = getResources().getDisplayMetrics().heightPixels;
+        float w = width / ww;
+        float h = height / wh;
+        ValueAnimator animator = ValueAnimator.ofFloat(Math.min(w, h), 1);
+        animator.setDuration(3000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float f = (float) animation.getAnimatedValue();
+                int w = (int) (ww * f);
+                int h = (int) (wh * f);
+                ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(w, h);
+                binding.image.setLayoutParams(layoutParams);
+                Log.e("aaa", "onAnimationUpdate: " + w + "/" + h);
+//                if (left >= ww / 2) {
+//
+//                } else {
+//                    if (ww - left == width / 2) {
+//
+//                    } else {
+//
+//                    }
+//                }
+            }
+        });
+        animator.start();
     }
 }
